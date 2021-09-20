7d0a62e91a55834fe1d27c3979ce1cb06861a283
Chris VanGroll <chrisvangroll@gmail.com>
2021-09-19 20:33:19 -0500

    Logo and navbar are finished

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav_link');
+const nav = document.querySelector('.nav');
 
 navToggle.addEventListener('click', () =>{
     document.body.classList.toggle('nav-open')
 })
+
+navLinks.forEach(link =>{
+    link.addEventListener('click', ()=>{
+        document.body.classList.remove('nav-open');
+    }) 
+});
